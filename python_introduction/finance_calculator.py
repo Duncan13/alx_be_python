e9c048a272633c1fc12466247f79c0d4bf90ebee
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-06-23 05:01:39 -0700

    added integer type casting

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -2,8 +2,8 @@ income = int(input("Enter your monthly income: "))
 expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = income - expenses
 
-print('Your monthly savings are $',monthly_savings)
+print(f'Your monthly savings are ${monthly_savings}')
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f'Projected savings after one year, with interest, is: ${projected_savings}')
+print(f'Projected savings after one year, with interest, is: ${int(projected_savings)}.')
