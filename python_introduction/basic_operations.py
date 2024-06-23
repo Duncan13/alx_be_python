88d9f687a14b7987962dc7af43a102ac30b670ea
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-06-23 02:15:46 -0700

    added python introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1+number2
+difference = number1-number2
+product = number1*number2
+
+print("Addition of 10 and 5 is",addition)
+print("Subtraction of 10 and 5 is",difference)
+print("Multiplication of 10 and 5 is",product)
