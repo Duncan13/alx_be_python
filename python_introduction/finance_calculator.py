e94b9395e16b70822b129c5bdf2aa3b25693622c
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-06-23 05:13:42 -0700

    changed variable names

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = income - expenses
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
 
 print(f'Your monthly savings are ${monthly_savings}.')
 
