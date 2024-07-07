3e189ab0671b7668807c1f9d3527ee296b7de0d3
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-07 11:36:01 -0700

    add a functions and data structures directory

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(operation,num1,num2):
+    if operation == 'add':
+        print(num1 + num2)
+    elif operation == 'subtract':
+        print(num1 - num2)
+    elif operation == 'multiply':
+        print(num1 * num2)
+    elif operation == 'divide':
+        if num1 == 0 or num2 == 0:
+            try:
+                return num1 / num2
+            except ZeroDivisionError:
+                print(0)
+        else:
+            print(num1/num2)
