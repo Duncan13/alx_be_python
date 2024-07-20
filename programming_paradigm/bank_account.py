a170d6f5f2ef0ab3bf77951068b6c5da2ae2fc98
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-20 08:30:01 -0700

    made some changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,9 +6,9 @@ class BankAccount:
         self.account_balance = self.account_balance+amount
 
     def withdraw(self,amount):
-        #if self.account_balance >= amount:
-        amount = 0
-        self.account_balance = self.account_balance - amount
+        amount = float(input(''))
+        if self.account_balance >= amount:
+            self.account_balance = self.account_balance - amount
         
 
     def display_balance(self):
