480a317bfd0241147a5507292d1de5465d9a0e61
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-20 08:01:44 -0700

    made some changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,12 +3,10 @@ class BankAccount:
         self.account_balance = 0
 
     def deposit(self,amount):
-        amount = 67.0
         self.account_balance = self.account_balance+amount
         print(f'Deposited: ${amount}')
 
     def withdraw(self,amount):
-        amount = 50.0
         if self.account_balance >= amount:
             self.account_balance = self.account_balance - amount
             print(f'Withdrew: ${amount}')
