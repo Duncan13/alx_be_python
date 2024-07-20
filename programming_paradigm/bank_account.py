4786cd92326940b9bfbe123354686065af4d48d9
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-20 11:15:12 -0700

    one more time

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,3 +1,4 @@
+'''
 class BankAccount:
     def __init__(self, account_balance):
         self.account_balance = 0
@@ -15,4 +16,25 @@ class BankAccount:
 
     def display_balance(self):
         return self.account_balance
+'''
+
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = 0
+
+    def deposit(self,amount):
+        #amount = int(input('Amount to be deposited: '))
+        self.account_balance = self.account_balance+amount
+       # print(f'Deposited: ${amount}')
+
+    def withdraw(self,amount):
+        amount = int(input('Amount to withdraw: '))
+        if self.account_balance >= amount:
+            self.account_balance = self.account_balance - amount
+            print(f'Amount withdrawn: ${amount}')
+        else:
+            print('Insufficient funds')
+
+    def display_balance(self):
+        print(f'Current Balance: ${self.account_balance}')
 
