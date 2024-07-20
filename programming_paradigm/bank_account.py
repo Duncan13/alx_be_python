7f19b2a591a97f17e253f3b1a8f6b9ef3f12833f
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-20 09:06:56 -0700

    made some changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,18 +1,18 @@
 class BankAccount:
     def __init__(self, account_balance):
-        self.account_balance = 0
+        self.account_balance = account_balance
 
     def deposit(self,amount):
         self.account_balance = self.account_balance+amount
 
     def withdraw(self,amount):
         if amount > self.account_balance:
-            return 'Insufficient funds'
+            return amount > self.account_balance
         else:
             self.account_balance = self.account_balance-amount
             return self.account_balance
         
 
     def display_balance(self):
-        return 'Current Balance: ${self.account_balance}'
+        return self.account_balance
 
