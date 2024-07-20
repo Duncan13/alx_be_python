628731ae3668844482361b2f9dc173b909f0274f
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-20 07:41:35 -0700

    made some changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,12 +2,12 @@ class BankAccount:
     def __init__(self, account_balance):
         self.account_balance = 0
 
-    def deposit(self):
+    def deposit(self,amount):
         amount = int(input('Amount to be deposited: '))
         self.account_balance = self.account_balance+amount
         print(f'Deposited: ${amount}')
 
-    def withdraw(self):
+    def withdraw(self,amount):
         amount = int(input('Amount to withdraw: '))
         if self.account_balance >= amount:
             self.account_balance = self.account_balance - amount
