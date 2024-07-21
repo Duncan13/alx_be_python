ed0ce95f7082a65d8bff91faad2e933536932351
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-21 09:11:04 -0700

    fixed the withdraw function4

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,47 +1,3 @@
-
-'''class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = 0
-
-    def deposit(self,amount):
-        self.account_balance = self.account_balance+amount
-
-    def withdraw(self,amount):
-        if amount > self.account_balance:
-            return amount > self.account_balance
-        else:
-            self.account_balance = self.account_balance-amount
-            return self.account_balance
-        
-
-    def display_balance(self):
-        return self.account_balance
-
-class BankAccount:
-    def __init__(self, account_balance):
-        self.account_balance = 0
-
-    def deposit(self,amount):
-        #amount = int(input('Amount to be deposited: '))
-        self.account_balance = self.account_balance+amount
-       # print(f'Deposited: ${amount}')
-
-    def withdraw(self,amount):
-        amount = 0 # int(input('Amount to withdraw: '))
-        if self.account_balance >= amount:
-        # if amount > self.account_balance:
-            self.account_balance = self.account_balance - amount
-            return False
-         else:
-           # self.account_balance -= amount
-            return self.account_balance
-            #return amount
-            #print('Insufficient funds')
-
-    def display_balance(self):
-        print(f'Current Balance: ${self.account_balance}')
-'''
-
 class BankAccount:
     def __init__(self, account_balance=0):
         self.account_balance = account_balance
@@ -51,6 +7,7 @@ class BankAccount:
         return self.account_balance
 
     def withdraw(self, amount):
+        amount = 0
         if amount > self.account_balance:
             return "Insufficient funds"
         elif self.account_balance >= amount:
