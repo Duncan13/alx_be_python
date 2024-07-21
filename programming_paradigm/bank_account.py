e2a1c523110325b07beae9257fbba58497ec5de8
Duncan Mufamadi <mufamadiduncan07@gmail.com>
2024-07-21 08:59:48 -0700

    fixed the withdraw function4

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -53,6 +53,8 @@ class BankAccount:
     def withdraw(self, amount):
         if amount > self.account_balance:
             return "Insufficient funds"
+        elif self.account_balance >= amount:
+            self.account_balance = self.account_balance - amount
         else:
             self.account_balance -= amount
             return self.account_balance
